a60a0b851912154a46b346dead23ca381e488f99
Raul Jimenez Ortega <rjimenez@esri.com>
2022-01-31 19:18:50 +0100

    Adding dynamic behaviour to calculator

--- js/calculator-app.js
+++ js/calculator-app.js
@@ -1,12 +1,184 @@
+function calculatePoints(event) {  
+    const inputs = [
+        'tipo_domicilio',
+        'discapacidad',
+        'fam_numerosa',
+        'fam_monoparental',
+        'parto_multiple',
+        'acogimiento',
+        'vic_genero'
+        // 'escolarizados',
+        // 'centro_w_Familiar'
+    ];
+    let inputValues = {};
+    
+    inputs.forEach(elem => {
+        inputValues[elem] = document.querySelector(`[name="${elem}"]`);
+    });
 
+    if(document.querySelector(`[name="escolarizados"]`).value === "si"){
+        inputValues["escolarizados"] = document.querySelectorAll(`[name^="centro_familiar_"]`);
+    }
+    
+    console.log(`Values: `, inputValues);
+    event.preventDefault();
+}
+
+function toggleAddSchoolBtn(){
+    const centrosField = document.querySelector('[name="centros"]'),
+          addSchoolBtn = document.getElementById("add-school")
+    if(centrosField.disabled === false){
+        centrosField.setAttribute("disabled", true);
+        addSchoolBtn.setAttribute("disabled", true);
+    }else{
+        centrosField.removeAttribute("disabled");
+        addSchoolBtn.removeAttribute("disabled")
+    }
+
+}
+function addSchoolField(event) {
+    const centrosField = document.querySelector('[name="centros"]')
+    
+    // Disable option an select next one
+    const centrosOption = centrosField.options[centrosField.selectedIndex];
+    centrosOption.setAttribute("disabled", true);
+    const start = centrosField.selectedIndex;
+    let optionDisabled = true;
+    do{
+        centrosField.selectedIndex = (centrosField.selectedIndex + 1) % centrosField.options.length;
+        if(centrosField.options[centrosField.selectedIndex].disabled != true){
+            optionDisabled = false;
+        }
+        if(centrosField.selectedIndex === start){
+            optionDisabled = false;
+            // console.log("Deshabilitar boton");
+            toggleAddSchoolBtn();            
+        }
+    }while(optionDisabled);
+
+    // Add HTML template
+    var newSchool = oneLineTag("div", {
+        class: "calculator-group__item",
+        id: `family_related_center_${start}`
+    });
+    newSchool.innerHTML = `
+        <p class="calculator-group__label">
+            En el ${centrosOption.innerText} tiene 
+        </p>
+        <select 
+            class="calculator-group__option" 
+            name="centro_familiar_${start}"
+            data-slug="${slugify(centrosOption.innerText)}"
+            data-schoolName="${centrosOption.innerText}"
+        >
+            <option value="null">-</option>
+            <option value="hermanos">Hermanos matriculados</option>
+            <option value="padres">Padre(s) trabajando</option>
+            <option value="ambos">Ambos</option>
+        </select>
+        <button data-schoolIndex="${start}">
+            Borrar
+        </button>
+    `;
+
+    document.querySelector('.calculator-group__set').appendChild(newSchool);
+
+    const newBtnEl = document.querySelector(`#family_related_center_${start} button`);
+    newBtnEl.addEventListener('click', removeSchoolField);  
+    
+    // console.log(htmlTemplate)
+    event.preventDefault();
+}
+
+function removeSchoolField(event){
+    // Remove school and restore option
+    const notDisabled = document.querySelectorAll('[name="centros"] option:not([disabled=true])');
+    if(notDisabled.length === 0){
+        toggleAddSchoolBtn()
+    }
+    const index = parseInt(event.target.dataset.schoolindex);
+    const centrosField = document.querySelector('[name="centros"]')
+    centrosField.options[index].removeAttribute("disabled")
+    document.getElementById(`family_related_center_${index}`).remove();
+    event.preventDefault();
+}
+
+
+
+function oneLineTag(tag,options){
+    return Object.assign(document.createElement(tag),options);
+}
+
+function waitForSchools(){
+    if(allSchools && allSchools.length !== 0){
+        const centrosField = document.querySelector('[name="centros"]');
+        allSchools.forEach(elem => {
+            const opt = oneLineTag("option", {
+                innerText: elem.attributes.Nombre,
+                value: slugify(elem.attributes.Nombre)
+            });
+            centrosField.appendChild(opt)
+            
+        });
+    }
+    else{
+        setTimeout(waitForSchools, 250);
+    }
+}
+
+// Slugify a string
+function slugify(str)
+{
+    str = str.replace(/^\s+|\s+$/g, '');
+
+    // Make the string lowercase
+    str = str.toLowerCase();
+
+    // Remove accents, swap ñ for n, etc
+    var from = "ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆÍÌÎÏŇÑÓÖÒÔÕØŘŔŠŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇíìîïňñóöòôõøðřŕšťúůüùûýÿžþÞĐđßÆa·/_,:;";
+    var to   = "AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAa------";
+    for (var i=0, l=from.length ; i<l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    // Remove invalid chars
+    str = str.replace(/[^a-z0-9 -]/g, '') 
+    // Collapse whitespace and replace by -
+    .replace(/\s+/g, '-') 
+    // Collapse dashes
+    .replace(/-+/g, '-'); 
+
+    return str;
+}
 
 function Main() {
     console.log('estamos en Main ');
     // Aqui ponemos el flujo normal que lleva la aplicacion. 
     
+    const calculatorForm = document.getElementById('calculator-form');
+    //   const log = document.getElementById('log');
+    calculatorForm.addEventListener('submit', calculatePoints);  
+
+    
+    // We wait until schools are ready
+    waitForSchools();
+    
+    // Add dynamic behaviour to add family related schools
+    const addCenter = document.getElementById('add-school');
+    addCenter.addEventListener('click', addSchoolField);  
+
+    
+    var select = document.querySelector('#linked-to-school');
+    select.addEventListener('change',function(evt){
+        const divEl = document.querySelector('.calculator-group');
+        
+        if (divEl.style.display === "none") {
+            divEl.style.display = "block";
+        } else {
+            divEl.style.display = "none";
+        }
+    });
+
 }
-
-
-
 
 Main();
---
+++ js/map.js
@@ -0,0 +1,206 @@
+import { intersects } from "https://js.arcgis.com/4.18/@arcgis/core/geometry/geometryEngineAsync.js";
+const apiKey = "AAPK24e42b66a3b54203a7a77df57d25822atOmmoxEQT_himZgqKMjLhMkagFjnZjooh9EPm6fACROkuVbnG9V3I7wGQRZRmJ4O";
+
+const authentication = new arcgisRest.ApiKey({
+    key: apiKey
+});
+
+function processForm(e) {
+    if (e.preventDefault) e.preventDefault();
+
+    arcgisRest.geocode({
+        address: document.getElementById("address").value,
+        postal: 13700,
+        city: "Tomelloso",
+        countryCode: "ES",
+        // searchExtent: "-3.1247949550328133,39.10051392185686,-2.901906068104559,39.22949157768225",
+        authentication
+    })
+        .then((response) => {
+            console.log("Candidates:", response.candidates);
+            if (response.candidates[0].address == 13700) {
+                elem = document.getElementById("info-center")
+                elem.classList.remove('success');
+                elem.classList.add('error');
+                document.getElementById("info-center").innerHTML = "Lo sentimos, no hemos sido capaces de encontrar tu dirección"
+                return -1;
+            }
+            // Reload map
+            var data = {
+                'webmap': '1db22a3fd99b4f9dab2c9432cf0f1d2d',
+                'zoom': true,
+                'scale': true,
+                'basemap_toggle': true,
+                'alt_basemap': 'hybrid',
+                'disable_scroll': true,
+                'theme': 'light',
+            };
+
+            var string_ = JSON.stringify(data);
+
+            string_ = string_.replace(/{/g, "");
+            string_ = string_.replace(/}/g, "");
+            string_ = string_.replace(/:/g, "=")
+            string_ = string_.replace(/,/g, "&");
+            string_ = string_.replace(/"/g, "");
+
+            const markerData = {
+                description: 'Asegúrate de que el mapa haya ubicado correctamente tu dirección',
+                url: 'https://tomellosotech.maps.arcgis.com/sharing/rest/content/items/587ee7c1f9fd41d297d8955e21bf113e/data',
+                title: response.candidates[0].address
+            }
+            let marker = '' +
+                `${response.candidates[0].location.x};` +
+                `${response.candidates[0].location.y};` +
+                `${response.candidates[0].location.spatialReference.wkid};` +
+                `${encodeURIComponent(markerData.description)};` +
+                `${encodeURIComponent(markerData.url)};` +
+                `${encodeURIComponent(markerData.title)}`;
+
+            // string_+= `&center=${response.candidates[0].location.x},${response.candidates[0].location.y}`;
+            string_ += '&extent=' +
+                `${response.candidates[0].extent.xmin};` +
+                `${response.candidates[0].extent.ymin};` +
+                `${response.candidates[0].extent.xmax};` +
+                `${response.candidates[0].extent.ymax}`;
+            string_ += '&marker=' + marker;
+
+            document.getElementById("dashboard").src = `//tomellosotech.maps.arcgis.com/apps/Embed/index.html?${string_}`
+
+            bordering = [];
+            arcgisRest.queryFeatures({
+                url: "https://services7.arcgis.com/d9R4ThD32qsG1Wu4/ArcGIS/rest/services/ZonasEscolares/FeatureServer/0",
+                geometry: response.candidates[0].location,
+                geometryType: "esriGeometryPoint",
+                spatialRel: "esriSpatialRelIntersects"
+            })
+                .then(response => {
+                    console.log(response.features);
+                    let elem = document.getElementById("info-center")
+                    elem.classList.add('success');
+                    elem.classList.remove('error');
+                    // document.getElementById("info-center").innerHTML = `Hemos ubicado tu calle en la ${response.features[0].attributes.Nombre}, por favor revisa en el mapa que la hemos ubicado correctamente.<br> Los centros de Infantil y Primaria que se encuentran en esta zona son: <br>${response.features[0].attributes.Puntos}`;
+
+                    const addressZone = response.features[0];
+                    addressZone.geometry.spatialReference = {
+                        "wkid": 25830
+                    };
+
+                    function getBorderingZones() {
+                        return new Promise(resolve => {
+                            allZones.forEach(async function (zone, i) {
+                                zone.spatialReference = {
+                                    "wkid": 25830
+                                }
+                                const isBordering = await intersects(addressZone.geometry, zone.geometry);
+                                if (isBordering === true) {
+                                    if (response.features[0].attributes.Nombre != zone.attributes.Nombre) {
+                                        bordering.push(zone)
+                                    }
+                                }
+                                if (i === allZones.length - 1) {
+                                    // console.log("Terminamos y devolvemos promesa getBorderingZones()")
+                                    resolve(bordering);
+                                }
+                            });
+                        });
+                    }
+
+                    function getBorderingSchools() {
+                        return new Promise(resolve => {
+                            getBorderingZones().then(borderingZones => {
+                                let bordering = [];
+                                borderingZones.forEach((zone, i) => {
+                                    allSchools.forEach(async function (school, j) {
+                                        school.geometry.spatialReference = zone.geometry.spatialReference = {
+                                            "wkid": 25830
+                                        };
+                                        const isBordering = await intersects(school.geometry, zone.geometry);
+                                        if (isBordering === true) {
+                                            // console.log(`${zone.attributes.Nombre} si intersecta con ${school.attributes.Nombre} `)
+                                            bordering.push(school)
+                                        } else {
+                                            // console.log(`${zone.attributes.Nombre} no intersecta con ${school.attributes.Nombre} `)
+                                        }
+
+                                        if (i === borderingZones.length - 1 && j === allSchools.length - 1) {
+                                            resolve(bordering);
+                                        }
+                                    });
+                                });
+                            });
+
+                        });
+                    }
+
+                    getBorderingSchools().then(borderingSchools => {
+                        const borderingSchoolsStr = borderingSchools.reduce((previousValue, currentValue) => {
+                            if (previousValue.attributes) {
+                                previousValue = "<br> &bull; " + previousValue.attributes.Nombre
+                            }
+                            return previousValue += `<br> &bull; ${currentValue.attributes.Nombre}`;
+                        });
+                        document.getElementById("info-center").innerHTML = `Hemos ubicado tu calle en la ${response.features[0].attributes.Nombre}, por favor revisa en el mapa que la hemos ubicado correctamente.<br><br>Los centros de Infantil y Primaria que se encuentran en esta zona son: <br>${response.features[0].attributes.Puntos}<br><br>Y los de las zonas limítrofes son: ${borderingSchoolsStr}.`;
+                    })
+
+                });
+        });
+
+    /*
+
+    const centers = ['San Isidro', 'Moral']
+    const getLocationPoint = function ....(){
+        const localizacion = {
+            centers[0]: 10,
+            centers[1]: 8,
+            centers[2]: 0
+        };
+        return localizacion;
+    }
+
+    if (discapacidad.value == "padre") {
+        discapacidad = 3
+    }
+
+    total = {}
+    while () {
+        total.center[i] = localizacion[] + discapacidad //+ ...;
+    }
+    */
+
+
+    return false;
+}
+
+function MainMap() {
+
+  arcgisRest.queryFeatures({
+    url: "https://services7.arcgis.com/d9R4ThD32qsG1Wu4/ArcGIS/rest/services/ZonasEscolares/FeatureServer/0"
+  }).then(response => {
+      // Fill global variable with all zones
+      allZones = response.features;
+      // console.log("Todas las zonas =", allZones);
+  });
+
+  arcgisRest.queryFeatures({
+      url: "https://services.arcgis.com/Q6ZFRRvMTlsTTFuP/ArcGIS/rest/services/Colegios_Tomelloso_2/FeatureServer/0",
+      where: "Clasificacion = 'Infantil y Primaria'",
+      orderByFields: "Nombre",
+      outSR: 25830
+  }).then(response => {
+      // Fill global variable with all schools
+      allSchools = response.features;
+      // console.log("Todos los coles =", allSchools);
+  });
+
+  var form = document.getElementById('query-form');
+  if(form){
+    if (form.attachEvent) {
+        form.attachEvent("submit", processForm);
+    } else {
+        form.addEventListener("submit", processForm);
+    }
+  }
+}
+
+MainMap();
